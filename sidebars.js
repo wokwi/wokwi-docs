6757adebd77a5e116abff16b6b2f0bb4e79dae77
Uri Shaked <uri@urishaked.com>
2021-04-19 16:26:17 +0300

    feat(wokwi-analog-joystick): analog joystick reference

--- sidebars.js
+++ sidebars.js
@@ -4,6 +4,7 @@ module.exports = {
     Guides: ['keyboard-shortcuts', 'gdb-debugging', 'guides/serial-monitor', 'guides/libraries'],
     'Diagram Reference': [
       'diagram-format',
+      'parts/wokwi-analog-joystick',
       'parts/wokwi-arduino-nano',
       'parts/wokwi-arduino-uno',
       'parts/wokwi-attiny85',
