336210f416a654260569d3f7267c6e510986ff01
TillDV <mail@tilldv.de>
2023-09-17 18:01:47 +0200

    updated config again (yes im dumb)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -127,7 +127,7 @@ module.exports = {
   ],
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'pt-BR', 'zh-CN'],
+    locales: ['en', 'pt-BR', 'zh-CN', 'de-DE'],
     localeConfigs: {
       en: {
         label: 'English',
